83ce3d9cb158078bd5f92f190d46fdc634c18778
Aldo Espinosa <aldoespinosaperez1@gmail.com>
2025-04-24 16:50:46 -0600

    Change table name in Product class

--- src/main/java/com/aldo/ecommerce_challenge/models/Product.java
+++ src/main/java/com/aldo/ecommerce_challenge/models/Product.java
@@ -1,11 +1,9 @@
 package com.aldo.ecommerce_challenge.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "products")
 public class Product {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
