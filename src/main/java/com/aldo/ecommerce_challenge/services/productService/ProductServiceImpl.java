c9c72d0d2e0cc01a8dbf6ac87337ce7f560b0d40
Aldo Espinosa <aldoespinosaperez1@gmail.com>
2025-04-24 23:19:54 -0600

    Product service implementation

--- src/main/java/com/aldo/ecommerce_challenge/services/productService/ProductServiceImpl.java
+++ src/main/java/com/aldo/ecommerce_challenge/services/productService/ProductServiceImpl.java
@@ -1,33 +1,58 @@
 package com.aldo.ecommerce_challenge.services.productService;
 
 import com.aldo.ecommerce_challenge.models.Product;
+import com.aldo.ecommerce_challenge.repositories.ProductRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 public class ProductServiceImpl implements ProductService {
+  private final ProductRepository repository;
+
+  public ProductServiceImpl(ProductRepository repository) {
+    this.repository = repository;
+  }
+
   @Override
+  @Transactional(readOnly = true)
   public List<Product> findAll() {
-    return List.of();
+    return (List<Product>) this.repository.findAll();
   }
 
   @Override
-  public Optional<Product> findById() {
-    return Optional.empty();
+  @Transactional(readOnly = true)
+  public Optional<Product> findById(Long id) {
+    return this.repository.findById(id);
   }
 
   @Override
+  @Transactional
   public Product save(Product product) {
-    return null;
+    return this.repository.save(product);
   }
 
   @Override
   public Optional<Product> update(Long id, Product product) {
-    return Optional.empty();
+    return repository
+        .findById(id)
+        .map(
+            productDb -> {
+              productDb.setName(product.getName());
+              productDb.setDescription(product.getDescription());
+              productDb.setPrice(product.getPrice());
+              return this.repository.save(productDb);
+            });
   }
 
   @Override
   public Optional<Product> delete(Long id) {
-    return Optional.empty();
+    return repository
+        .findById(id)
+        .map(
+            product -> {
+              this.repository.delete(product);
+              return product;
+            });
   }
 }
