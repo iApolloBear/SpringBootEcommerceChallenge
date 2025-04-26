95c22c62d1c830111a6af5d9d99b484ab51cd7cc
Aldo Espinosa <aldoespinosaperez1@gmail.com>
2025-04-26 12:40:33 -0600

    Order controller endpoints

--- src/main/java/com/aldo/ecommerce_challenge/orders/controllers/OrderController.java
+++ src/main/java/com/aldo/ecommerce_challenge/orders/controllers/OrderController.java
@@ -3,6 +3,7 @@ package com.aldo.ecommerce_challenge.orders.controllers;
 import com.aldo.ecommerce_challenge.orderItems.models.OrderItem;
 import com.aldo.ecommerce_challenge.orders.models.Order;
 import com.aldo.ecommerce_challenge.orders.services.OrderService;
+import com.aldo.ecommerce_challenge.products.models.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,5 +35,22 @@ public class OrderController {
   @PostMapping
   public ResponseEntity<Order> create(@RequestBody List<OrderItem> orderItems) {
     return ResponseEntity.status(HttpStatus.CREATED).body(this.orderService.save(orderItems));
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Order> update(
+      @PathVariable Long id, @RequestBody List<OrderItem> orderItems) {
+    return this.orderService
+        .update(id, orderItems)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Order> delete(@PathVariable Long id) {
+    return this.orderService
+        .delete(id)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
   }
 }
