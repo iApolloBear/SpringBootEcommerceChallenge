99e9ac2f706fbf4276cd138373d9e8c5ceeee746
Aldo Espinosa <aldoespinosaperez1@gmail.com>
2025-04-28 00:44:11 -0600

    Get order items by ids.

--- src/main/java/com/aldo/ecommerce_challenge/orderItems/repositories/OrderItemRepository.java
+++ src/main/java/com/aldo/ecommerce_challenge/orderItems/repositories/OrderItemRepository.java
@@ -3,4 +3,8 @@ package com.aldo.ecommerce_challenge.orderItems.repositories;
 import com.aldo.ecommerce_challenge.orderItems.models.OrderItem;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrderItemRepository extends CrudRepository<OrderItem, Long> {}
+import java.util.List;
+
+public interface OrderItemRepository extends CrudRepository<OrderItem, Long> {
+  List<OrderItem> findByIdIn(List<Long> ids);
+}
