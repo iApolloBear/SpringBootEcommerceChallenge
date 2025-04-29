550b5c15d2dca64d51b13b1a4d4765bd7098dadc
Aldo Espinosa <aldoespinosaperez1@gmail.com>
2025-04-28 23:21:09 -0600

    Order Service tests.

--- src/test/java/com/aldo/ecommerce_challenge/orders/OrdersData.java
+++ src/test/java/com/aldo/ecommerce_challenge/orders/OrdersData.java
@@ -1,28 +1,51 @@
 package com.aldo.ecommerce_challenge.orders;
 
+import com.aldo.ecommerce_challenge.orderItems.dto.OrderItemDTO;
 import com.aldo.ecommerce_challenge.orderItems.models.OrderItem;
+import com.aldo.ecommerce_challenge.orders.dto.OrderDTO;
 import com.aldo.ecommerce_challenge.orders.models.Order;
 import com.aldo.ecommerce_challenge.products.ProductsData;
-import com.aldo.ecommerce_challenge.products.models.Product;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 public class OrdersData {
+  public static Optional<OrderDTO> createOrderDTOOne() {
+    OrderItemDTO orderItemDTO1 = new OrderItemDTO(1L, 1L, 1L, 1, new BigDecimal("938"));
+    OrderItemDTO orderItemDTO2 = new OrderItemDTO(2L, 1L, 2L, 1, new BigDecimal("1499"));
+    return Optional.of(
+        new OrderDTO(1L, new BigDecimal("2437"), List.of(orderItemDTO1, orderItemDTO2)));
+  }
+
+  public static Optional<OrderDTO> createOrderDTOTwo() {
+    OrderItemDTO orderItemDTO1 = new OrderItemDTO(3L, 2L, 1L, 2, new BigDecimal("1876"));
+    OrderItemDTO orderItemDTO2 = new OrderItemDTO(4L, 2L, 2L, 2, new BigDecimal("2998"));
+    return Optional.of(
+        new OrderDTO(2L, new BigDecimal("4874"), List.of(orderItemDTO1, orderItemDTO2)));
+  }
+
   public static Optional<Order> createOrderOne() {
     Order order = new Order();
     order.setId(1L);
-    Product product1 = ProductsData.createProductOne().orElseThrow();
-    Product product2 = ProductsData.createProductTwo().orElseThrow();
-    OrderItem orderItem1 = new OrderItem(order, product1, 2);
-    OrderItem orderItem2 = new OrderItem(order, product2, 3);
-    order.addItem(orderItem1);
-    order.addItem(orderItem2);
+    OrderItem orderItem1 =
+        new OrderItem(1L, order, ProductsData.createProductOne().orElseThrow(), 1);
+    OrderItem orderItem2 =
+        new OrderItem(2L, order, ProductsData.createProductTwo().orElseThrow(), 1);
+    order.setOrderItems(List.of(orderItem1, orderItem2));
+    order.setTotal(new BigDecimal("2437"));
     return Optional.of(order);
   }
 
   public static Optional<Order> createOrderTwo() {
     Order order = new Order();
     order.setId(2L);
+    OrderItem orderItem1 =
+        new OrderItem(3L, order, ProductsData.createProductOne().orElseThrow(), 2);
+    OrderItem orderItem2 =
+        new OrderItem(4L, order, ProductsData.createProductTwo().orElseThrow(), 2);
+    order.setOrderItems(List.of(orderItem1, orderItem2));
+    order.setTotal(new BigDecimal("4874"));
     return Optional.of(order);
   }
 }
--- src/test/java/com/aldo/ecommerce_challenge/orders/services/OrderServiceImplTest.java
+++ src/test/java/com/aldo/ecommerce_challenge/orders/services/OrderServiceImplTest.java
@@ -1,107 +1,136 @@
 package com.aldo.ecommerce_challenge.orders.services;
 
+import com.aldo.ecommerce_challenge.orderItems.dto.OrderItemDTO;
 import com.aldo.ecommerce_challenge.orderItems.models.OrderItem;
+import com.aldo.ecommerce_challenge.orderItems.repositories.OrderItemRepository;
 import com.aldo.ecommerce_challenge.orders.OrdersData;
+import com.aldo.ecommerce_challenge.orders.dto.OrderDTO;
+import com.aldo.ecommerce_challenge.orders.dto.OrderUpdateDTO;
+import com.aldo.ecommerce_challenge.orders.mappers.OrderMapper;
 import com.aldo.ecommerce_challenge.orders.models.Order;
 import com.aldo.ecommerce_challenge.orders.repositories.OrderRepository;
 import com.aldo.ecommerce_challenge.products.ProductsData;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
+@DisplayName("OrderService Tests")
 public class OrderServiceImplTest {
-//  @MockitoBean OrderRepository orderRepository;
-//  @Autowired OrderService orderService;
-//
-//  @Test
-//  void testFindAll() {
-//    List<Order> data =
-//        List.of(
-//            OrdersData.createOrderOne().orElseThrow(), OrdersData.createOrderTwo().orElseThrow());
-//    when(this.orderService.findAll()).thenReturn(data);
-//    List<Order> orders = this.orderService.findAll();
-//    assertFalse(orders.isEmpty());
-//    assertEquals(2, orders.size());
-//    verify(this.orderRepository).findAll();
-//  }
-//
-//  @Test
-//  void testFindById() {
-//    when(this.orderService.findById(1L)).thenReturn(OrdersData.createOrderOne());
-//    when(this.orderService.findById(2L)).thenReturn(OrdersData.createOrderTwo());
-//    Optional<Order> order1 = this.orderService.findById(1L);
-//    Optional<Order> order2 = this.orderService.findById(2L);
-//    assertTrue(order1.isPresent());
-//    assertEquals(1, order1.get().getId());
-//    assertEquals("6373", order1.get().getTotal().toPlainString());
-//    assertTrue(order2.isPresent());
-//    assertEquals(2, order2.get().getId());
-//    assertEquals("0", order2.get().getTotal().toPlainString());
-//    assertFalse(this.orderService.findById(3L).isPresent());
-//    verify(this.orderRepository, times(3)).findById(anyLong());
-//  }
-//
-//  @Test
-//  void testSave() {
-//    Order newOrder = new Order();
-//    OrderItem orderItem = new OrderItem(newOrder, ProductsData.createProductOne().orElseThrow(), 1);
-//    when(this.orderRepository.save(any()))
-//        .then(
-//            invocation -> {
-//              Order o = invocation.getArgument(0);
-//              o.setId(3L);
-//              return o;
-//            });
-//    Order order = this.orderService.save(List.of(orderItem));
-//    assertEquals(3, order.getId());
-//    assertEquals(1, order.getOrderItems().size());
-//    assertEquals("938", order.getTotal().toPlainString());
-//    verify(this.orderRepository).save(any());
-//  }
-//
-//  @Test
-//  void testUpdate() {
-//    when(this.orderRepository.findById(1L)).thenReturn(OrdersData.createOrderOne());
-//    Optional<Order> order = this.orderService.findById(1L);
-//    assertTrue(order.isPresent());
-//    Order newOrder = new Order();
-//    OrderItem orderItem = new OrderItem(newOrder, ProductsData.createProductTwo().orElseThrow(), 2);
-//    newOrder.addItem(orderItem);
-//    when(this.orderRepository.save(any()))
-//        .then(
-//            invocation -> {
-//              Order o = invocation.getArgument(0);
-//              o.setId(1L);
-//              return o;
-//            });
-//    Optional<Order> result = this.orderService.update(1L, List.of(orderItem));
-//    assertTrue(result.isPresent());
-//    assertEquals(1, result.get().getId());
-//    assertEquals("2998", result.get().getTotal().toPlainString());
-//    assertEquals(1, result.get().getOrderItems().size());
-//    verify(this.orderRepository).save(any());
-//  }
-//
-//  @Test
-//  void testDelete() {
-//    when(this.orderRepository.findById(2L)).thenReturn(OrdersData.createOrderTwo());
-//    Optional<Order> order = this.orderService.findById(2L);
-//    assertTrue(order.isPresent());
-//    Optional<Order> result = this.orderService.delete(2L);
-//    assertTrue(result.isPresent());
-//    assertEquals(2, result.get().getId());
-//    assertEquals("0", result.get().getTotal().toPlainString());
-//
-//    verify(this.orderRepository, times(2)).findById(2L);
-//    verify(this.orderRepository).delete(order.get());
-//  }
+  @MockitoBean OrderRepository orderRepository;
+  @MockitoBean OrderMapper orderMapper;
+  @MockitoBean OrderItemRepository orderItemRepository;
+  @Autowired OrderService orderService;
+
+  @Test
+  @DisplayName("Should return all orders")
+  void testFindAll() {
+    Order order1 = OrdersData.createOrderOne().orElseThrow();
+    Order order2 = OrdersData.createOrderTwo().orElseThrow();
+    OrderDTO orderDTO = OrdersData.createOrderDTOOne().orElseThrow();
+    when(orderRepository.findAll()).thenReturn(List.of(order1, order2));
+    when(orderMapper.toDto(order1)).thenReturn(orderDTO);
+
+    List<OrderDTO> orders = orderService.findAll();
+
+    assertFalse(orders.isEmpty());
+    assertEquals(2, orders.size());
+    assertTrue(orders.contains(orderDTO));
+    verify(this.orderRepository).findAll();
+  }
+
+  @Test
+  @DisplayName("Find order by id successfully")
+  void testFindById() {
+    Optional<Order> order1 = OrdersData.createOrderOne();
+    Optional<Order> order2 = OrdersData.createOrderTwo();
+    when(orderRepository.findById(1L)).thenReturn(order1);
+    when(orderRepository.findById(2L)).thenReturn(order2);
+    when(orderMapper.toDto(order1.orElseThrow()))
+        .thenReturn(OrdersData.createOrderDTOOne().orElseThrow());
+    when(orderMapper.toDto(order2.orElseThrow()))
+        .thenReturn(OrdersData.createOrderDTOTwo().orElseThrow());
+
+    Optional<OrderDTO> result1 = orderService.findById(1L);
+    Optional<OrderDTO> result2 = orderService.findById(2L);
+
+    assertTrue(result1.isPresent());
+    assertEquals(1L, result1.get().getId());
+    assertTrue(result2.isPresent());
+    assertEquals(2L, result2.get().getId());
+
+    verify(this.orderRepository).findById(1L);
+    verify(this.orderRepository).findById(2L);
+  }
+
+  @Test
+  @DisplayName("Save new order successfully")
+  void testSave() {
+    Order order = new Order();
+    OrderDTO dto = new OrderDTO(3L, order.getTotal(), List.of());
+    when(orderRepository.save(any())).thenReturn(order);
+    when(orderMapper.toDto(order)).thenReturn(dto);
+
+    OrderDTO result = orderService.save();
+
+    assertEquals(3L, result.getId());
+    assertTrue(result.getOrderItems().isEmpty());
+    assertEquals("0", result.getTotal().toPlainString());
+    verify(this.orderRepository).save(order);
+  }
+
+  @Test
+  @DisplayName("Update order successfully")
+  void testUpdate() {
+    when(this.orderRepository.findById(1L)).thenReturn(Optional.of(new Order()));
+    Optional<Order> order = this.orderRepository.findById(1L);
+    assertTrue(order.isPresent());
+    OrderItem orderItem1 =
+        new OrderItem(1L, order.get(), ProductsData.createProductOne().orElseThrow(), 1);
+    OrderItem orderItem2 =
+        new OrderItem(2L, order.get(), ProductsData.createProductTwo().orElseThrow(), 1);
+    order.get().addItem(orderItem1);
+    order.get().addItem(orderItem2);
+    OrderUpdateDTO orderUpdateDTO =
+        new OrderUpdateDTO(List.of(orderItem1.getId(), orderItem2.getId()));
+    when(this.orderRepository.save(any())).thenReturn(order.get());
+    when(this.orderItemRepository.findByIdIn(List.of(1L, 2L)))
+        .thenReturn(List.of(orderItem1, orderItem2));
+    when(this.orderMapper.toDto(order.get()))
+        .thenReturn(OrdersData.createOrderDTOOne().orElseThrow());
+    Optional<OrderDTO> result = orderService.update(1L, orderUpdateDTO);
+
+    assertTrue(result.isPresent());
+    assertEquals(1, result.get().getId());
+    assertEquals(2, result.get().getOrderItems().size());
+    assertEquals("2437", result.get().getTotal().toPlainString());
+    verify(this.orderRepository).save(any());
+    verify(this.orderItemRepository).findByIdIn(List.of(1L, 2L));
+  }
+
+  @Test
+  @DisplayName("Delete order successfully")
+  void delete_ShouldDeleteOrder() {
+    Optional<Order> order = OrdersData.createOrderOne();
+    Optional<OrderDTO> orderDTO = OrdersData.createOrderDTOOne();
+    when(orderRepository.findById(1L)).thenReturn(order);
+    when(orderMapper.toDto(order.orElseThrow())).thenReturn(orderDTO.orElseThrow());
+
+    Optional<OrderDTO> result = orderService.delete(1L);
+
+    assertTrue(result.isPresent());
+    assertEquals(1L, result.get().getId());
+
+    verify(this.orderRepository).findById(1L);
+    verify(this.orderRepository).delete(order.get());
+  }
 }
